16a00b350cd437fea4be049cb90717773c5b4219
cauphenuny <starseekist@gmail.com>
2025-04-22 17:13:28 +0800

    fix: link libc dynamically

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cat head.s block{1,2,3,4}.s >count.s
 as --32 count.s -o count.o
-ld -lc -m elf_i386 count.o -o count
+ld -lc -dynamic-linker /lib/ld-linux.so.2 -m elf_i386 count.o -o count
