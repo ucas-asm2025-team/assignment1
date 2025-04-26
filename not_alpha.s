925d8b9e6c2e20a3d678d4254ffed053015fac0b
Alpha. <167853416+forever000000@users.noreply.github.com>
2025-04-27 00:17:13 +0800

    Update not_alpha.s

--- not_alpha.s
+++ not_alpha.s
@@ -1,5 +1,32 @@
 .globl not_alpha
 
 .section .text
+.type not_alpha, @function
+
 not_alpha:
+    pushl %ebp
+    movl  %esp, %ebp
+
+    
+    movl  cur_id, %eax
+    testl %eax, %eax
+    jz    .exit                             
+
+   
+    leal  cnt(,%eax,4), %esi              
+    incl  (%esi)                           
+    movl  max_cnt, %ebx
+    cmpl  %ebx, (%esi)
+    jle   .no_update
+    movl  (%esi), %ebx                      
+    movl  %ebx, max_cnt
+    movl  %eax, max_id                      
+
+.no_update:
+  
+    movl  $0, cur_id
+
+.exit:
+    leave
     ret
+    
