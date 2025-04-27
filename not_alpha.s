ff04e9c08d25f0ec8ac25eafa06e3a76b71e1bd9
yuejiuge <2742772327@qq.com>
2025-04-27 17:23:16 +0800

    Update not_alpha.s

--- not_alpha.s
+++ not_alpha.s
@@ -1,42 +1,48 @@
 .globl not_alpha
 
 .section .text
+.code32
 .type not_alpha, @function
 
 not_alpha:
-    pushq %rbp
-    movq %rsp, %rbp
-    pushq %rbx                    # 保存%rbx寄存器
+    push %ebp
+    movl %esp, %ebp
+    push %ebx                    # 保存%ebx寄存器
 
-    # 通过GOT访问全局变量（64位PIC）
-    movq cur_id@GOTPCREL(%rip), %rax  # 获取cur_id的地址
-    movl (%rax), %eax            # 读取cur_id的值
+    call .get_got                # 获取当前指令地址到栈
+.get_got:
+    pop %ebx                     # %ebx = 当前地址
+    addl $_GLOBAL_OFFSET_TABLE_ + (.get_got - not_alpha), %ebx  # 计算GOT基地址
+
+    # 通过GOT访问全局变量
+    movl cur_id@GOT(%ebx), %eax  # 获取cur_id的地址
+    movl (%eax), %eax            # 读取cur_id的值
     testl %eax, %eax
     jz .exit
 
     # 处理cnt数组
-    movq cnt@GOTPCREL(%rip), %rsi     # 获取cnt数组基地址
-    leaq (%rsi, %rax, 4), %rsi        # 计算cnt[cur_id]地址
-    incl (%rsi)                       # cnt[cur_id]++
+    movl cnt@GOT(%ebx), %esi     # 获取cnt数组基地址
+    leal (%esi, %eax, 4), %esi   # 计算cnt[cur_id]地址
+    incl (%esi)                  # cnt[cur_id]++
 
     # 处理max_cnt
-    movq max_cnt@GOTPCREL(%rip), %rcx # 获取max_cnt的地址
-    movl (%rcx), %edx                 # 读取max_cnt的值
-    cmpl %edx, (%rsi)
+    movl max_cnt@GOT(%ebx), %ecx # 获取max_cnt的地址
+    movl (%ecx), %edx            # 读取max_cnt的值
+    cmpl %edx, (%esi)
     jle .no_update
 
-    movl (%rsi), %edx                 # 更新max_cnt
-    movl %edx, (%rcx)
-    movq max_id@GOTPCREL(%rip), %rcx  # 获取max_id的地址
-    movl %eax, (%rcx)
+    movl (%esi), %edx            # 更新max_cnt
+    movl %edx, (%ecx)
+    movl max_id@GOT(%ebx), %ecx  # 获取max_id的地址
+    movl %eax, (%ecx)
 
 .no_update:
-    movq cur_id@GOTPCREL(%rip), %rcx  # 获取cur_id的地址
-    movl $0, (%rcx)                   # cur_id = 0
+    movl cur_id@GOT(%ebx), %ecx  # 获取cur_id的地址
+    movl $0, (%ecx)              # cur_id = 0
 
 .exit:
-    popq %rbx                        # 恢复%rbx
-    leaveq
-    retq
-
+    pop %ebx                     # 恢复%ebx
+    leave
+    ret
+    
     
