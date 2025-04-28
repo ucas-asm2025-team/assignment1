0054d7766d9134f037bc312a43d91a9e296dc58a
cauphenuny <starseekist@gmail.com>
2025-04-28 08:27:15 +0800

    fix: main.s: use relative addressing for global vars; save rsi register before call output

--- main.s
+++ main.s
@@ -6,11 +6,11 @@
 _start:
 while_cond:
     call    getchar
-    movl    %eax, ch # let ch = getchar()
+    movl    %eax, ch(%rip) # let ch = getchar()
     cmpl    $-1, %eax
     je      while_end # jump ch == EOF ? while_end : if_cond
 if_cond:
-    movl    ch, %edi
+    movl    ch(%rip), %edi
     call    isalpha
     testl   %eax, %eax
     jz      else_stmt # jump isalpha(ch) ? if_stmt : else_stmt
@@ -23,13 +23,17 @@ else_stmt:
 if_end:
     jmp while_cond # end of loop, jump to condition
 while_end:
-    movl    $0, %esi # let counter %esi = 0
+    movq    $0, %rsi # let counter %rsi = 0
 for_loop:
-    cmpl    max_siz, %esi
+    cmpl    max_siz(%rip), %esi
     jge     for_end # if %esi >= max_siz then break
-    movl    max_id(,%esi,4), %edi # let %edi = max_id[%esi]
+    leaq    max_id(%rip), %rdi # let %rdi = &max_id
+    movl    (%rdi,%rsi,4), %edi # let %edi = max_id[%esi]
+    movslq  %edi, %rdi # sign extend %edi to %rdi
+    push    %rsi
     call    output # call output(max_id[%esi])
-    incl    %esi # %esi++
+    pop     %rsi
+    inc     %rsi # %esi++
     jmp     for_loop
 for_end:
 # if linked with c source debug.c, the debug() func will print all tree nodes
