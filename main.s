dd9a21dd5545a3ba5e4dec4ebaddbaa47d008130
cauphenuny <starseekist@gmail.com>
2025-04-28 23:37:13 +0800

    fix: let temp labels in `main.s` be local

--- main.s
+++ main.s
@@ -4,29 +4,29 @@
 
 .section .text
 _start:
-while_cond:
+.while_cond:
     call    getchar
     movl    %eax, ch(%rip) # let ch = getchar()
     cmpl    $-1, %eax
-    je      while_end # jump ch == EOF ? while_end : if_cond
-if_cond:
+    je      .while_end # jump ch == EOF ? while_end : if_cond
+.if_cond:
     movl    ch(%rip), %edi
     call    isalpha
     testl   %eax, %eax
-    jz      else_stmt # jump isalpha(ch) ? if_stmt : else_stmt
-if_stmt:
+    jz      .else_stmt # jump isalpha(ch) ? if_stmt : else_stmt
+.if_stmt:
     call    alpha # call alpha()
-    jmp     if_end
-else_stmt:
+    jmp     .if_end
+.else_stmt:
     call    not_alpha # call not_alpha()
-    jmp     if_end
-if_end:
-    jmp while_cond # end of loop, jump to condition
-while_end:
+    jmp     .if_end
+.if_end:
+    jmp .while_cond # end of loop, jump to condition
+.while_end:
     movq    $0, %rsi # let counter %rsi = 0
-for_loop:
+.for_loop:
     cmpl    max_siz(%rip), %esi
-    jge     for_end # if %esi >= max_siz then break
+    jge     .for_end # if %esi >= max_siz then break
     leaq    max_id(%rip), %rdi # let %rdi = &max_id
     movl    (%rdi,%rsi,4), %edi # let %edi = max_id[%esi]
     movslq  %edi, %rdi # sign extend %edi to %rdi
@@ -34,8 +34,8 @@ for_loop:
     call    output # call output(max_id[%esi])
     pop     %rsi
     inc     %rsi # %esi++
-    jmp     for_loop
-for_end:
+    jmp     .for_loop
+.for_end:
 # if linked with c source debug.c, the debug() func will print all tree nodes
 # otherwise, debug() will do nothing, just serves as a place holder.
     call    debug
